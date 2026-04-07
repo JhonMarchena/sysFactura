6dabcb9f931c8347950e13e380cf82e51d5a3d30
JhonMarchena <jhcronos@gmail.com>
2026-04-07 22:55:56 +0200

    ...

--- client/src/components/inputs.tsx
+++ client/src/components/inputs.tsx
@@ -1,18 +1,31 @@
-import { Input } from "antd";
+import { Input, Button} from "antd";
 
 interface InputProps {
   placeholder?: string;
   icon?: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+export function InputButton(props: InputProps) {
+  return (
+    <div className="w-[80%] ">
+      <Button className="custom-button">{props.placeholder}</Button>
+    </div>
+  );
 }
 
 export function InputIcon(props: InputProps) {
   return (
-    <div className="w-[25rem]">
+    <div className="w-[80%]">
       <Input placeholder={props.placeholder} prefix={props.icon} />
     </div>
   );
 }
 
-export function InputBasic(props: InputProps) {
-  return <Input placeholder={props.placeholder} prefix={props.icon} />;
+export function InputPassword(props: InputProps) {
+  return (
+    <div className="w-[80%]">
+      <Input.Password placeholder={props.placeholder} />
+    </div>
+  );
 }
--- client/src/login/presentation/components/loginCard.tsx
+++ client/src/login/presentation/components/loginCard.tsx
@@ -1,22 +1,26 @@
 import { FaUser } from "react-icons/fa";
 import { UserOutlined } from "@ant-design/icons";
-import { InputBasic, InputIcon } from "../../../components/inputs";
+import { InputPassword, InputButton, InputIcon } from "../../../components/inputs";
 
 function loginCard() {
   return (
     <div className="flex relative bg-white rounded-xl w-[60%] h-[70%]">
-      <div className="flex items-center justify-center  bg-[var(--secondary-color)] w-1/3 h-full rounded-l-xl ">
+      <div className="flex items-center justify-center bg-[var(--secondary-color)] w-1/3 h-full rounded-l-xl ">
         <FaUser className="text-[var(--neutral-color)] text-9xl" />
       </div>
       <div className="flex bg-[var(--subsecondary-color)] flex-col w-2/3 h-full rounded-r-xl">
-      <h1>Iniciar Sesión</h1>
-        <div>
-      
-          <InputIcon placeholder="Nombre de usuario" icon={<UserOutlined />} />
-        </div>
 
-        <div>
-          <InputBasic placeholder="Contraseña" />
+        <div className="flex flex-col p-6 gap-y-10 items-center justify-center h-full w-full">
+          <h1 className="text-3xl font-bold text-white">Iniciar Sesión</h1>
+
+          <div className="flex flex-col gap-4 w-[80%] items-center justify-center">
+            <InputIcon placeholder="Nombre de usuario" icon={<UserOutlined />} />
+            <InputPassword placeholder="Contraseña"/>
+          </div>
+
+          <div>
+            <InputButton placeholder="Iniciar Sesión"/>
+          </div>
         </div>
       </div>
     </div>
